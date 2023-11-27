2069b103effcc193a0bc6fd962a34d466037f426
olia <olga.uk@gmail.com>
2023-11-27 22:22:36 +0200

    Added randomizer feature, bugfix, removed some steps

--- src/test/java/com/ignitisTest/stepdefinitions/utils/TestUtils.java
+++ src/test/java/com/ignitisTest/stepdefinitions/utils/TestUtils.java
@@ -6,20 +6,18 @@ public class TestUtils {
         WebElement elementToFind;
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         while (true) {
-            // Try to find the element
             try {
                 elementToFind = driver.findElement(By.xpath(elementXpath));
                 if (elementToFind.isDisplayed()) {
                     return Optional.of(elementToFind);
                 }
             } catch (NoSuchElementException e) {
-                // If element is not found, scroll down and retry
                 jsExecutor.executeScript("window.scrollBy(0, 100);"); // Scroll down by 100 pixels
             }
             boolean isScrolledToEnd = (boolean) jsExecutor.executeScript(
                     "return window.innerHeight + window.scrollY >= document.body.offsetHeight");
             if (isScrolledToEnd) {
-                break; // Stop the loop if we've reached the end of the page
+                break;
             }
         }
         return Optional.empty();
